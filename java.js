c31f01540a534449d559db4edc1df652114188d4
n0sej0b <garrett.l.rogers@gmail.com>
2024-09-12 19:25:37 -0600

    Updated Portfolio

--- java.js
+++ java.js
@@ -9,3 +9,33 @@ const navSlide = () => {
 }
 
 navSlide ();
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+        slides[i].classList.remove('mySlidesfade') 
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block"; 
+    slides[slideIndex-1].classList.add('mySlidesfade') 
+    dots[slideIndex-1].className += " active";
+}
